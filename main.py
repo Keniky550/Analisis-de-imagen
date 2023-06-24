4ce64e758af153b7a046cf548972bc01fc074e2e
keniky550 <ken.jav550@gmil.com>
2023-06-23 21:25:48 -0500

    Solo faltan los colores

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ventana.title("Características del objeto")
 etiqueta1 = tk.Label(ventana, text="Forma:")
 combobox1 = ttk.Combobox(ventana,values=["circulo", "cuadrado", "triangulo", "rectangulo","pentagono", "hexagono"])
 etiqueta2 = tk.Label(ventana, text="Color:")
-combobox2 = ttk.Combobox(ventana,values=["rojo", "naranja", "amarillo", "verde","rosa","violeta"])
+combobox2 = ttk.Combobox(ventana,values=["rojo", "azul", "naranja", "amarillo", "verde","rosa","violeta"])
 boton_guardar = tk.Button(ventana, text="Guardar", command=guardar_datos)
 
 etiqueta1.pack()
@@ -31,7 +31,7 @@ boton_guardar.pack()
 ventana.mainloop()
 #Fin del input
 
-
+#Parametrizamos los colores
 # Rojo
 rojoBajo1 = np.array([0, 100, 20], np.uint8)
 rojoAlto1 = np.array([10, 255, 255], np.uint8)
@@ -58,13 +58,19 @@ violetaAlto = np.array([145, 255, 255], np.uint8)
 rosaBajo = np.array([146, 100, 20], np.uint8)
 rosaAlto = np.array([170, 255, 255], np.uint8)
 
+#Azul
+azulBajo = np.array([100,100,20],np.uint8)
+azulAlto = np.array([125,255,255],np.uint8)
+
+#Iniciar variables
 img = cv2.imread("figurasColores2.png")
 imagenHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 output = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
-circulo = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-detectar_circulo = np.uint16(np.around(circulo))
+canny = cv2.Canny(gray,10,150)
+canny = cv2.dilate(canny,None,iterations=1)
+canny = cv2.erode(canny,None,iterations=1)
 
 # Se buscan los colores en la imagen, segun los límites altos 
 # y bajos dados
@@ -76,6 +82,7 @@ maskAmarillo = cv2.inRange(imagenHSV, amarilloBajo, amarilloAlto)
 maskVerde = cv2.inRange(imagenHSV, verdeBajo, verdeAlto)
 maskVioleta = cv2.inRange(imagenHSV, violetaBajo, violetaAlto)
 maskRosa = cv2.inRange(imagenHSV, rosaBajo, rosaAlto)
+maskAzul = cv2.inRange(imagenHSV,azulBajo,azulAlto)
 
 #Encontrar contornos
 contornoAmarillo = cv2.findContours(maskAmarillo, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -84,19 +91,42 @@ contornoVerde = cv2.findContours(maskVerde, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_S
 contornoVioleta = cv2.findContours(maskVioleta, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 contornoNaranja = cv2.findContours(maskNaranja, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 contornoRosa= cv2.findContours(maskRosa,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+contornoAzul = cv2.findContours(maskAzul, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 
-if color == "rojo": cv2.drawContours(img,contornoRojo,-1,(0,255,0),2)
-if color == "naranja": cv2.drawContours(img,contornoNaranja,-1,(0,255,0),2)
-if color == "amarillo": cv2.drawContours(img,contornoAmarillo,-1,(0,255,0),2)
-if color == "verde": cv2.drawContours(img,contornoVerde,-1,(0,255,0),2)
-if color == "violeta": cv2.drawContours(img,contornoVioleta,-1,(0,255,0),2)
-if color == "rosa": cv2.drawContours(img,contornoRosa,-1,(0,255,0),2)
+#Trabajando con las figuras
+ctns,_=cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+for c in ctns:
+    epsilon = 0.015*cv2.arcLength(c,True)
+    approx = cv2.approxPolyDP(c,epsilon,True)
+    print(len(approx))
+    #cv2.drawContours(img,[approx],0,(0,255,0),2)
+    #cv2.imshow("Figuras", img)
+    #cv2.waitKey(0)
+    if forma == 'triangulo' and len(approx) == 3:
+        if color == "": cv2.drawContours(img,[approx],-1,(0,255,0),2)
+        elif color == "rojo": cv2.drawContours(img,contornoRojo,-1,(0,255,0),2)
+        elif color == "naranja": cv2.drawContours(img,contornoNaranja,-1,(0,255,0),2)
+        elif color == "amarillo": cv2.drawContours(img,contornoAmarillo,-1,(0,255,0),2)
+        elif color == "verde": cv2.drawContours(img,contornoVerde,-1,(0,255,0),2)
+        elif color == "violeta": cv2.drawContours(img,contornoVioleta,-1,(0,255,0),2)
+        elif color == "rosa": cv2.drawContours(img,contornoRosa,-1,(0,255,0),2)
+        elif color == "azul": cv2.drawContours(img,contornoAzul,-1,(0,255,0),2)
+    elif forma == 'circulo' and len(approx) == 8:
+        if color == "": cv2.drawContours(img,[approx],-1,(0,255,0),2)
+    elif forma == 'cuadrado' and len(approx) == 4:
+        if color == "": cv2.drawContours(img,[approx],-1,(0,255,0),2)
+    elif forma == 'hexagono' and len(approx) == 6:
+        if color == "": cv2.drawContours(img,[approx],-1,(0,255,0),2)
+    elif forma == 'pentagono' and len(approx) == 5:
+        if color == "": cv2.drawContours(img,[approx],-1,(0,255,0),2)
+    
 
 
-#if forma == "circulo":
-#	for(x,y,r) in detectar_circulo[0, :]:
-#		cv2.circle(output,(x,y),r,(0,255,0),2)
 
-cv2.imshow("Figuras", img)
+
+
+
+cv2.imshow("FIguras", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
